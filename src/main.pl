e6e80f102359932a17470ba6029b0db8ad021aec
maartenvn <maartenvn.eu@gmail.com>
2021-04-13 21:42:37 +0200

    feat(main): update main cli using the new parser

--- src/main.pl
+++ src/main.pl
@@ -1,10 +1,14 @@
 :- initialization main.
-:- [parser].
+:- use_module("io/parser").
 
 main(_):-
     % Load the data from the stdin stream and parse it into a board.
-    phrase_from_stream(parse_board(Board), current_input),
+    phrase_from_stream(parser:parse_board(Board, Rokades, StartColor), current_input),
 
     write(Board),
+    write("\n"),
+    write(Rokades),
+    write("\n"),
+    write(StartColor),
 
     halt(0).
