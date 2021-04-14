b4fd2316cfa526a7e9c26d0ab3a36d6c506a8152
maartenvn <maartenvn.eu@gmail.com>
2021-04-14 11:29:22 +0200

    feat(parser): improve parser & allow for better re-use in the writer

--- src/io/parser.pl
+++ src/io/parser.pl
@@ -2,42 +2,53 @@
 
 :- use_module(library(pio)).
 :- use_module(library(dcg/basics)).
+
+% Interpret quoted strings as ASCII character codes.
 :- set_prolog_flag(double_quotes, codes).
 
-test :-
-    phrase_from_file(parse_rows(Pieces, Rokades, StartColor), "test_chars.txt", [encoding(utf8),type(text)]),
-    write(Pieces),
-    write(Rokades),
-    write(StartColor).
 
 %! parse_board(-Board, -Rokades, -StartColor)
 %
 %  Parse a chess board.
 parse_board(Board, Rokades, StartColor) --> 
-    parse_rows(Board, Rokades, StartColor),
-    parse_final_row.
+    parse_rows(8, Pieces, RokadesList, StartColor),
+    parse_final_row,
+    {
+        % Create a flat list of pieces to create the board
+        append(Pieces, Board),
+
+        % Create a flat list of rokades to create the board
+        append(RokadesList, Rokades)
+    }.
 
 
-%! parse_rows(-Pieces)
+%! parse_rows(+Y, -Pieces, -Rokades, -StartColor)
 %
 %  Parse all rows in a flat list of board positions.
 %  TODO: this could be cleaned up by a recursive statement
-parse_rows(Pieces, Rokades, StartColor) -->
-    parse_border_row(8, PiecesRow8, RokadesBlack, StartColor),
-    parse_row(7, PiecesRow7),
-    parse_row(6, PiecesRow6),
-    parse_row(5, PiecesRow5),
-    parse_row(4, PiecesRow4),
-    parse_row(3, PiecesRow3),
-    parse_row(2, PiecesRow2),
-    parse_border_row(1, PiecesRow1, RokadesWhite, StartColor),
-    {
-        % Merge rokades
-        append([RokadesBlack, RokadesWhite], Rokades),
+parse_rows(8, [Pieces | PiecesRest], [Rokades | RokadesRest], StartColor) --> % Last row
 
-        % Merge pieces
-        append([PiecesRow1, PiecesRow2, PiecesRow3, PiecesRow4, PiecesRow5, PiecesRow6, PiecesRow7, PiecesRow8], Pieces)
-    }.
+    % Parse the row
+    parse_border_row(8, black, Pieces, Rokades, StartColor),
+
+    % Recursive call
+    parse_rows(7, PiecesRest, RokadesRest, StartColor), !.
+
+parse_rows(1, [Pieces], [Rokades], StartColor) --> % First row
+
+    % Parse the row
+    parse_border_row(1, white, Pieces, Rokades, StartColor), !.
+
+parse_rows(Y, [Pieces | PiecesRest], Rokades, StartColor) --> % Rows in between first & last row
+    {
+        YNext is Y - 1
+    },
+
+    % Parse the row
+    parse_row(Y, Pieces),
+    
+    % Recursive call
+    parse_rows(YNext, PiecesRest, Rokades, StartColor), !.
 
 
 %! parse_row(-Y, -Pieces)
@@ -51,53 +62,66 @@ parse_row(Y, Pieces) -->
     parse_newline, !.
 
 
-%! parse_border_row(-Y, -Pieces, -Rokades, -StartColor)
+%! parse_border_row(+Y, +Color, -Pieces, -Rokades, -StartColor)
 %
 %  Parse the first/last row of the chess board.
-parse_border_row(Y, Pieces, Rokades, StartColor) -->
+parse_border_row(Y, Color, Pieces, Rokades, StartColor) -->
     parse_row_number(Y),
     parse_space,
     parse_pieces(1/Y, Pieces),
     parse_space,
-    parse_rokades(Rokades),
+    parse_rokades(Color, Rokades),
     parse_current_player(Y, StartColor),
-    parse_newline.
+    parse_newline, !.
 
 
-%! parse_rokades(-Rokades)
+%! parse_rokades(+Color, -Rokades)
 %
 %  Parse a rokade notation.
-parse_rokades(Rokades) -->
+%  
+%  This is verbose on purpose to allow re-use of the parser for generating output.
+parse_rokades(Color, [LongRokade, ShortRokade]) --> % Both rokades
     "[",
-    parse_rokade_piece(LongRokades),
-    parse_rokade_piece(ShortRokades),
-    {
-        % Merge short & long rokades
-        append([LongRokades, ShortRokades], Rokades)
-    },
-    "]".
+    parse_rokade_piece(Color, long, LongRokade),
+    parse_rokade_piece(Color, short, ShortRokade),
+    "]", !.
 
-%! parse_rokade_piece(-Rokades)
+parse_rokades(Color, [LongRokade]) --> % Only first rokade
+    "[",
+    parse_rokade_piece(Color, long, LongRokade),
+    parse_space,
+    "]", !.
+
+parse_rokades(Color, [ShortRokade]) --> % Only second rokade
+    "[",
+    parse_space,
+    parse_rokade_piece(Color, short, ShortRokade),
+    "]", !.
+
+parse_rokades(_, []) --> % No rokades
+    "[",
+    parse_space,
+    parse_space,
+    "]", !.
+
+%! parse_rokade_piece(+Color, +RokadeType, -Rokades)
 %
-%  Parse a single piece of the rokade notation or a space.
-parse_rokade_piece([Rokade]) --> % Large
-    parse_piece(Color, queen),
+%  Parse a single piece of the rokade notation
+parse_rokade_piece(Color, long, Rokade) --> % Large
+    parse_piece(Color, queen), 
 
     % Create the rokade
     {
-        Rokade = rokade(Color, large)
+        Rokade = rokade(Color, long)
     }, !.
 
-parse_rokade_piece([Rokade]) --> % Short
-    parse_piece(Color, king),
+parse_rokade_piece(Color, short, Rokade) --> % Short
+    parse_piece(Color, king), 
 
     % Create the rokade
     {
-        Rokade = rokade(Color, small)
+        Rokade = rokade(Color, short)
     }, !.
-
-parse_rokade_piece(_) --> % Space
-    parse_space, !.
 
 
 %! parse_current_player(+Y, -StartColor)
@@ -141,7 +165,7 @@ parse_newline --> "\n".
 %  Will stop parsing when a newline is detected
 
 % Piece: taken position
-parse_pieces(X/Y, [Piece|Pieces]) -->
+parse_pieces(X/Y, [Piece | Pieces]) -->
     {
         XNext is X + 1,
 
@@ -153,8 +177,8 @@ parse_pieces(X/Y, [Piece|Pieces]) -->
         between(1, 8, X)
     },
 
-    parse_piece(Color, Type), !,
-    parse_pieces(XNext/Y, Pieces).
+    parse_piece(Color, Type), 
+    parse_pieces(XNext/Y, Pieces), !.
 
 % Space: empty position
 parse_pieces(X/Y, Pieces) -->
@@ -167,8 +191,9 @@ parse_pieces(X/Y, Pieces) -->
     },
 
     parse_space,
-    parse_pieces(XNext/Y, Pieces).
-parse_pieces(_, []) --> [].
+    parse_pieces(XNext/Y, Pieces), !.
+
+parse_pieces(_, []) --> [], !.
 
 
 %! parse_piece(-Color, -Type)
@@ -178,12 +203,12 @@ parse_piece(white, king)   --> "\u2654", !. % White king
 parse_piece(white, queen)  --> "\u2655", !. % White queen
 parse_piece(white, tower)  --> "\u2656", !. % White tower
 parse_piece(white, bishop) --> "\u2657", !. % White tower
-parse_piece(white, bishop) --> "\u2658", !. % White horse
+parse_piece(white, horse)  --> "\u2658", !. % White horse
 parse_piece(white, pawn)   --> "\u2659", !. % White pawn
 
 parse_piece(black, king)   --> "\u265A", !. % Black king
 parse_piece(black, queen)  --> "\u265B", !. % Black queen
 parse_piece(black, tower)  --> "\u265C", !. % Black tower
 parse_piece(black, bishop) --> "\u265D", !. % Black tower
-parse_piece(black, bishop) --> "\u265E", !. % Black horse
+parse_piece(black, horse)  --> "\u265E", !. % Black horse
 parse_piece(black, pawn)   --> "\u265F", !. % Black pawn
