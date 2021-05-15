7bbd47df0057cb5e290ed08bf857145304008d1c
maartenvn <maartenvn.eu@gmail.com>
2021-05-15 14:19:15 +0200

    test(position): add position tests

--- src/util/utils.pl
+++ src/util/utils.pl
@@ -19,3 +19,12 @@ between2(Start, Value) :-
     Minus is Start - 2,
     Plus  is Start + 2,
     between(Minus, Plus, Value).
+
+%! list_equals(+List1, +List2)
+%
+%  If 2 lists match, ignoring the order
+list_equals([], []).
+list_equals([H1 | T1], List2) :- 
+    member(H1, List2),
+    delete(List2, H1, List3),
+    list_equals(T1, List3).
