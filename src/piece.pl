cfeec011589437882732aa0d393542dba6205317
maartenvn <maartenvn.eu@gmail.com>
2021-06-14 19:15:37 +0200

    refactor(piece): remove unnecessary cuts

--- src/piece.pl
+++ src/piece.pl
@@ -25,23 +25,23 @@ color(piece(Color, _, _), Color).
 %! rokades_piece(+Piece, -Rokades)
 %
 %  List of rokades for a given piece
-rokades_piece(piece(white, tower, 1/1), [rokade(white, long)]) :- !.                      % Tower
-rokades_piece(piece(white, tower, 8/1), [rokade(white, short)]) :- !.                     % Tower
-rokades_piece(piece(white, king, 5/1), [rokade(white, long), rokade(white, short)]) :- !. % King
+rokades_piece(piece(white, tower, 1/1), [rokade(white, long)]).                      % Tower
+rokades_piece(piece(white, tower, 8/1), [rokade(white, short)]).                     % Tower
+rokades_piece(piece(white, king, 5/1), [rokade(white, long), rokade(white, short)]). % King
 
-rokades_piece(piece(black, tower, 1/8), [rokade(black, long)]) :- !.                      % Tower
-rokades_piece(piece(black, tower, 8/8), [rokade(black, short)]) :- !.                     % Tower
-rokades_piece(piece(black, king, 5/8), [rokade(black, long), rokade(black, short)]) :- !. % King
+rokades_piece(piece(black, tower, 1/8), [rokade(black, long)]).                      % Tower
+rokades_piece(piece(black, tower, 8/8), [rokade(black, short)]).                     % Tower
+rokades_piece(piece(black, king, 5/8), [rokade(black, long), rokade(black, short)]). % King
 
-rokades_piece(_, []) :- !. % Base case
+rokades_piece(_, []). % Base case
 
 
 %! passant_piece(+Piece, -Passant)
 %
 %  En-passant possibility for a given piece
-passant_piece(piece(white, pawn, X/4), passant(white, X/3)) :- !.
-passant_piece(piece(black, pawn, X/5), passant(black, X/6)) :- !.
-passant_piece(_, none) :- !.
+passant_piece(piece(white, pawn, X/4), passant(white, X/3)).
+passant_piece(piece(black, pawn, X/5), passant(black, X/6)).
+passant_piece(_, none).
 
 
 %! row_pieces(+Y, +Pieces, -RowPieces)
@@ -58,7 +58,6 @@ row_pieces(Y, [Piece | Pieces], [RowPiece | RowPieces]) :- % Match
 
     % Recursive call
     row_pieces(Y, Pieces, RowPieces), !.
-
 row_pieces(Y, [Piece | Pieces], RowPieces) :- % No match
     position(Piece, _/PieceY),
     
@@ -67,8 +66,7 @@ row_pieces(Y, [Piece | Pieces], RowPieces) :- % No match
 
     % Recursive call
     row_pieces(Y, Pieces, RowPieces), !.
-
-row_pieces(_, [], []) :- !.
+row_pieces(_, [], []).
 
 
 %! sorted_pieces/2(+Pieces, -SortedPieces)
@@ -102,7 +100,7 @@ sorted_pieces(Pieces, X, SortedPieces) :-
     % Recursive call
     XNext is X + 1,
     sorted_pieces(Pieces, XNext, SortedPieces), !.
-sorted_pieces(_, _, []) :- !.
+sorted_pieces(_, _, []).
 
 
 %! opponent(+Color, -OpponentColor)
